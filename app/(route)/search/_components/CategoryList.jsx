fa0f31a4583aa0e2339ab84d651040ce4e1b4361
mostafa osama <mostafaelshrombably775@gmail.com>
2024-06-20 16:57:50 +0300

    @import

--- app/(route)/search/_components/CategoryList.jsx
+++ app/(route)/search/_components/CategoryList.jsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 import useFetchData from "@/app/_hooks/useFetchData";
 import { MenuIcon } from "lucide-react";
+import { useState } from "react";
 function CategoryList() {
   const params = usePathname();
   const category = params.split("/")[2];
@@ -21,45 +22,47 @@ function CategoryList() {
   const [activeMenu, setActiveMenu] = useState(false);
 
   return (
-    <div className="h-screen fixed mt-5 flex flex-col">
+    <>
       <MenuIcon
         onClick={() => setActiveMenu(!activeMenu)}
         className="md:hidden flex cursor-pointer"
       />
-      {activeMenu && (
-        <Command>
-          <CommandInput placeholder="Type a command or search..." />
-          <CommandList className="overflow-visible">
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Suggestions">
-              {categoryList &&
-                categoryList.map((item, index) => (
-                  <CommandItem key={index}>
-                    <Link
-                      href={"/search/" + item?.attributes?.name}
-                      className={`p-2 flex gap-2
+      <div className="h-screen fixed mt-5 flex flex-col">
+        {activeMenu && (
+          <Command>
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList className="overflow-visible">
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Suggestions">
+                {categoryList &&
+                  categoryList.map((item, index) => (
+                    <CommandItem key={index}>
+                      <Link
+                        href={"/search/" + item?.attributes?.name}
+                        className={`p-2 flex gap-2
                 text-[14px]
                 text-blue-600
                 items-center
                 rounded-md cursor-pointer w-full
                 ${category == item.attributes.name && "bg-blue-100"}
                 `}
-                    >
-                      <Image
-                        src={item.attributes?.icon?.data.attributes?.url}
-                        alt="icon"
-                        width={25}
-                        height={25}
-                      />
-                      <label>{item.attributes.name}</label>
-                    </Link>
-                  </CommandItem>
-                ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      )}
-    </div>
+                      >
+                        <Image
+                          src={item.attributes?.icon?.data.attributes?.url}
+                          alt="icon"
+                          width={25}
+                          height={25}
+                        />
+                        <label>{item.attributes.name}</label>
+                      </Link>
+                    </CommandItem>
+                  ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        )}
+      </div>
+    </>
   );
 }
 
