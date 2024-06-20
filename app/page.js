f812086d3b08d2cef746f0f3f0798d33cb10655f
mostafa osama <mostafaelshrombably775@gmail.com>
2024-06-20 14:07:46 +0300

    page

--- app/page.js
+++ app/page.js
@@ -4,18 +4,13 @@ import CategorySearch from "./_components/CategorySearch";
 import DoctorList from "./_components/DoctorList";
 import GlobalApi from "./_utils/GlobalApi";
 import { useEffect, useState } from "react";
+import useFetchData from "./_hooks/useFetchData";
 
 export default function Home() {
-  const [doctorList, setDoctorList] = useState([]);
-  useEffect(() => {
-    getDoctorList();
-  }, []);
-  const getDoctorList = () => {
-    GlobalApi.getDoctorList().then((resp) => {
-      console.log(resp.data.data);
-      setDoctorList(resp.data.data);
-    });
-  };
+  // const [doctorList, setDoctorList] = useState([]);
+
+  const { data: doctorList } = useFetchData(GlobalApi.getDoctorList);
+
   return (
     <div>
       {/* Hero Section  */}
